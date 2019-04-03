52ade804e938c4b3dcb977bbdc47f520cf314533
dongsj <232171773@qq.com>
2019-04-03 16:21:33 +0800

    use Math.Clamp to limit the position.x

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
     [SerializeField] float screenWidthInUnits;
-    
+    [SerializeField] float unitSize;
+
     // Update is called once per frame
     void Update() {
         // Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
-        Vector2 paddlePosition = new Vector2(Input.mousePosition.x / Screen.width * screenWidthInUnits,transform.position.y);
+        Vector2 paddlePosition =
+            new Vector2(Input.mousePosition.x / Screen.width * screenWidthInUnits, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(paddlePosition.x, unitSize, screenWidthInUnits - unitSize);
         transform.position = paddlePosition;
     }
 }
