73a497b03d70708be5ae20ad1613e8b16bc52455
dongsj <232171773@qq.com>
2019-04-03 16:17:20 +0800

    finish move paddle with mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    [SerializeField] float screenWidthInUnits;
+    
+    // Update is called once per frame
+    void Update() {
+        // Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        Vector2 paddlePosition = new Vector2(Input.mousePosition.x / Screen.width * screenWidthInUnits,transform.position.y);
+        transform.position = paddlePosition;
+    }
+}
