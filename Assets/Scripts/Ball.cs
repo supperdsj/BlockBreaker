13c3e96147e6066891a07705918a07e19afdfe9d
dongsj <232171773@qq.com>
2019-04-03 17:39:10 +0800

    launch the ball finish

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 public class Ball : MonoBehaviour {
     [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     void Start() {
         paddleToBallVector = transform.position - paddle.transform.position;
     }
 
     void Update() {
+        if (!hasStarted) {
+            LaunchOnMouseClick();
+            LockBallToPaddle();
+        }
+    }
+
+    void LaunchOnMouseClick() {
+        if (Input.GetMouseButtonDown(0)) {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity=new Vector2(xPush,yPush);
+        }
+    }
+
+    void LockBallToPaddle() {
         Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
     }
