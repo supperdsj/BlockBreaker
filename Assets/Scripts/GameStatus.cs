a9af2ab459f54c15c5cf57baa08d90b19b25d3fd
dongsj <232171773@qq.com>
2019-04-04 14:54:27 +0800

    set Time.timeScale

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour {
+    [Range(0.5f,2f)] [SerializeField] float gameSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Time.timeScale = gameSpeed;
+    }
+}
